ca30e2a99a158e0d7f4c34621c7d61c42ee365b4
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-29 14:14:57 -0300

    refactor: refactor requirement 1 to reduce it complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     students_counter = 0
-    try:
-        for student_period in permanence_period:
-            if type(target_time) != int or type(student_period[0]) != int or type(student_period[1]) != int:
-                raise ValueError
-            if student_period[0] <= target_time <= student_period[1]:
-                students_counter += 1
-        return students_counter
-    except ValueError:
+    if type(target_time) != int:
         return None
+    for student_period in permanence_period:
+        if type(student_period[0]) != int or type(student_period[1]) != int:
+            return None
+        if student_period[0] <= target_time <= student_period[1]:
+            students_counter += 1
+    return students_counter
