0774dc0ab5932259403561767a73c4d0ad8c5913
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-29 16:34:28 -0300

    feat: adds requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,8 +2,4 @@ def is_palindrome_iterative(word):
     """ Faça o código aqui. """
     if len(word) == 0:
         return False
-    if len(word) <= 2 and word[0] == word[-1]:
-        return True
-    if word[0] != word[-1]:
-        return False
-    return is_palindrome_iterative(word[1:-1])
+    return word == word[::-1]
