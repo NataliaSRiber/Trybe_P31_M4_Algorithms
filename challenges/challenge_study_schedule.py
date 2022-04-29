4a4d09dcbdb1be10e80cc00f01a09849f7178ec2
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-04-29 14:00:16 -0300

    feat: adds requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    students_counter = 0
+    try:
+        for student_period in permanence_period:
+            if type(target_time) != int or type(student_period[0]) != int or type(student_period[1]) != int:
+                raise ValueError
+            if student_period[0] <= target_time <= student_period[1]:
+                students_counter += 1
+        return students_counter
+    except ValueError:
+        return None
